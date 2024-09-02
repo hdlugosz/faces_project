5315a74ca1c3b3a7052533c1775f9f259f114d22
Hubert Długosz <79986639+hdlugosz@users.noreply.github.com>
2024-09-02 22:57:05 +0200

    Update rename_to_date.py

--- rename_to_date.py
+++ rename_to_date.py
@@ -2,44 +2,44 @@ import os
 from PIL import Image, ExifTags
 from datetime import datetime
 
-# Ścieżka do katalogu ze zdjęciami
+# Path to the directory containing images
 input_folder = './<<input_folder>>'
-# Ścieżka do katalogu, w którym chcesz zapisać przetworzone zdjęcia
+# Path to the directory where you want to save the processed images
 output_folder = './<<output_folder>>'
 
-# Utwórz katalog wyjściowy, jeśli nie istnieje
+# Create the output directory if it does not exist
 os.makedirs(output_folder, exist_ok=True)
 
-# Funkcja do odczytywania daty z metadanych zdjęcia
+# Function to read the date from the image metadata
 def get_capture_date(image):
     try:
         for tag, value in image._getexif().items():
             if ExifTags.TAGS.get(tag) == 'DateTimeOriginal':
                 return value
     except Exception as e:
-        print(f"Nie można odczytać daty. Błąd: {e}")
+        print(f"Could not read the date. Error: {e}")
     return None
 
 i=1
-# Przetwarzanie każdego pliku w katalogu
+# Processing each file in the directory
 for filename in os.listdir(input_folder):
     print(i)
-    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):  # Sprawdź, czy plik jest obrazem
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):  # Check if the file is an image
         img_path = os.path.join(input_folder, filename)
         img = Image.open(img_path)
 
-        # Odczytując datę z zdjęcia
+        # Reading the date from the image
         capture_date = get_capture_date(img)
         if capture_date:
-            # Konwertuj datę na format yyyymmdd HHMMSS
+            # Convert the date to the format yyyymmdd HHMMSS
             date_object = datetime.strptime(capture_date, '%Y:%m:%d %H:%M:%S')
-            new_filename = f'IMG_{date_object.strftime("%Y%m%d")}_{date_object.strftime("%H%M%S")}.jpg'  # Dodaj IMG_ przed datą i godziną
+            new_filename = f'IMG_{date_object.strftime("%Y%m%d")}_{date_object.strftime("%H%M%S")}.jpg'  # Add IMG_ before the date and time
             output_path = os.path.join(output_folder, new_filename)
 
-            # Zapisz zdjęcie do nowego katalogu z nową nazwą
+            # Save the image to the new directory with the new name
             img.save(output_path)
-            print(f"Zapisano: {output_path}")
+            print(f"Saved: {output_path}")
         else:
-            print(f"Nie znaleziono daty dla: {filename}")
+            print(f"No date found for: {filename}")
     i=i+1
-print("Przetwarzanie zakończone!")
+print("Processing completed!")
